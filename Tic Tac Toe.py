8bc8b0315ae06c8eb8da1a862991d0a69b3ec026
VaughnPinnock <vpinnock@byui.edu>
2022-01-12 21:59:33 -0500

    commit on 'master'

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -17,21 +17,122 @@ The program must have more than one function.
 The program must have a function called main.
 
 """
-boardvalues = { '1':'', '2':'', '3':'',
-                '4':'', '5':'', '6':'',
-                '7':'', '8':'', '9':'',
-}
 
-def printboard():
-    print ('1' + '|' + '2' + '|' + '3')
-    print ('-+-+-+-')
-    print ('4' + '|' + '5' + '|' + '6')
-    print ('-+-+-+-')
-    print ('7' + '|' + '8' + '|' + '9')
+class Board():
+    def __init__(self):
+        self.cells = [" "," "," "," "," "," "," "," "," "," "]
+        
+    def display(self):
+        print (f'{self.cells[1]} | {self.cells[2]} | {self.cells[3]}')
+        print ('-+-+-+-+-')
+        print (f'{self.cells[4]} | {self.cells[5]} | {self.cells[6]}')
+        print ('-+-+-+-+-')
+        print (f'{self.cells[7]} | {self.cells[8]} | {self.cells[9]}')
+
+    def update_move(self, cell_no, player):
+        if self.cells[cell_no] == " ":
+            self.cells[cell_no] = player
+
+    def is_winner(self, player):
+        if self.cells[1]== player and self.cells[2]==player and self.cells[3]==player:
+            return True
+
+        if self.cells[4]== player and self.cells[5]==player and self.cells[6]==player:
+            return True
+
+        if self.cells[7]== player and self.cells[8]==player and self.cells[9]==player:
+            return True
+
+        if self.cells[1]== player and self.cells[4]==player and self.cells[7]==player:
+            return True
+
+        if self.cells[2]== player and self.cells[5]==player and self.cells[6]==player:
+            return True
+
+        if self.cells[3]== player and self.cells[6]==player and self.cells[9]==player:
+            return True
+
+        if self.cells[1]== player and self.cells[5]==player and self.cells[9]==player:
+            return True
+
+        if self.cells[3]== player and self.cells[5]==player and self.cells[7]==player:
+            return True
+        
+        return False
+
+    def is_draw(self):
+        used_cells = 0
+        for cell in self.cells:
+            if cell != " ":
+                used_cells += 1
+        if used_cells == 9:
+            return True
+        else:
+            return False
+         
+board = Board()
+
+def print_header():
+    print('Welcome to CS210 TIC-TAC-TOE\n')
+
+def refresh_screen():
+    # print_header()
+
+    
+    board.display()
 
 
 def main():
-    printboard()
+
+    
+    while True:
+
+        # refresh screen
+        refresh_screen()
+
+        # gets user choice
+        x_choice = int(input("\nx's turn to choose a square (1-9): "))
+
+        # update board
+        board.update_move(x_choice, 'X')
+
+        # refresh screen
+        refresh_screen() 
+
+        # check for an x winner
+        if board.is_winner('X'):
+            print ('\nX wins!')
+            break
+        
+        # check for an O winner
+        if board.is_winner('O'):
+            print ('\n0 wins!')
+            break
+            
+        if board.is_draw():
+            print ('\nTie Game\n')
+            break
+
+
+
+        # gets user choice
+        o_choice = int(input("\nO's turn to choose a square (1-9): "))
+
+        # updates board
+        board.update_move(o_choice, '0')
+
+        # check for an x winner
+        if board.is_winner('X'):
+            print ('\nX wins!')
+            break
+        
+        if board.is_winner('O'):
+            print ('\n0 wins!')
+            break
+
+
+  
+    
 
 if __name__ == "__main__":
     main()
